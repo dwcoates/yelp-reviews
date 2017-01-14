73d79ec5786e77a46d86dda675651bd0e5fae34c
dwcoates <dodge.w.coates@gmail.com>
2017-01-14 17:14:46 -0500

    Fix csvification and header parsing.

--- preprocess.py
+++ preprocess.py
@@ -1,42 +1,71 @@
+#! /usr/bin/python2.7
 import json
-from pprint import pprint
+import csv
 
 
-def findkey(obj, key):
+def _findkey(obj, key):
     if key in obj:
         return obj[key]
     for k, v in obj.items():
         if isinstance(v, dict):
-            ret = findkey(v, key)
-            print ret
-            return ret if ret is not None else ""
-        return ""
+            value = _findkey(v, key)
+            if value is not None:
+                return value
 
 
-def findallkeys(obj, noinc):
+def findkey(obj, key):
+    v = _findkey(obj, key)
+    return v if v is not None else ""
+
+
+def _findallkeys(obj, noinc):
     """
-    Find all keys in this obj
+    Find all terminal keys in this obj
     """
     ret = []
     for k in obj.iterkeys():
         if noinc is not None and k not in noinc:
             if isinstance(obj[k], dict):
-                ret += [k] + findallkeys(obj[k], noinc)
-            ret += [k]
-
+                ret += _findallkeys(obj[k], noinc)
+            else:
+                ret += [k]
     return ret
 
 
-def get_ascii(string):
+def findallkeys(obj, noinc):
+    return map(str, _findallkeys(obj, noinc))
+
+
+def allkeys(ff, noinc=[]):
     """
-    Temp?
+    Return the list of unique keys in the dataset.
+    This will then be reduced to a list of keys to look for in the json.
+
+    GREAT CARE TO BE TAKEN WITH THIS FUNCTION!!
+
+    If there are conflicting keynames in the dataset's json (at
+    different levels, presumably), this can have disastrously bad
+    effects on the csvification. Because python likes to scramble dict
+    keyname orders, this means that iterating over the keys to locate
+    values in the json objects (to tranlate a json object to a csv
+    line) will be undefined; it could grab the value for either of the
+    conflicting keys on a given findkey call.
     """
-    if isinstance(string, str):
-        return True
-    elif isinstance(string, unicode):
-        return False
-    else:
-        return str(string)
+    with open(ff) as dataset:
+        j = json.loads(dataset.readline().strip())
+        s1 = set(findallkeys(j, noinc))
+        for line in dataset:
+            j = json.loads(line.strip())
+            s1.union(set(findallkeys(j, noinc)))
+
+    return list(s1)
+
+
+def coerce_to_string(s):
+    try:
+        return str(s).encode("ascii")
+    except:
+        return ""
 
 
 def csvify_to_file(headers, infile, outfile):
@@ -44,54 +73,40 @@ def csvify_to_file(headers, infile, outfile):
     Flatten the json entry. This is pretty simple, and relies on a bit
     of preprocessing of the json keys to make sure there are no conflictions.
     """
-    with open(infile, "r") as ins, open(outfile, "w") as outs:
-        outs.write(",".join(headers) + "\n")  # write header to top of file
+    with open(infile, "r") as ins, open(outfile, "wb") as outs:
+        csvwriter = csv.writer(outs)
+        csvwriter.writerow(headers)  # write header to top of file
         for line in ins:
             j = json.loads(line)
             ents = []
             for h in headers:
-                k = findkey(j, h)
-                ents += str(k) if get_ascii(k) else ""
-            outs.write(",".join(ents) + "\n")
+                ents += [coerce_to_string(findkey(j, h))]
+            csvwriter.writerow(ents)
 
-# headers just enumerate what to flatten to csv. Assumes distinction
-# across fields.
-bus_headers = ['city', 'review_count', 'name', 'neighborhoods',
-               'type', 'business_id', 'full_address', 'state',
-               'longitude', 'stars', 'latitude', 'attributes',
-               'Take-out', 'Drive-Thru', 'Outdoor Seating', 'Caters',
-               'Noise Level', 'Delivery', 'Attire', 'Has TV',
-               'Price Range', 'Takes Reservations', 'Waiter Service',
-               'Accepts Credit Cards', 'Good for Kids', 'Good For Groups',
-               'Alcohol', 'attributes', 'open', 'categories']
-csvify_to_file(bus_headers,
-               "data/json_origin/yelp_academic_dataset_business.json",
-               "data/business.csv")
 
-checkin_headers = ['checkin_info', '9-5', '7-5', '13-3', '17-6',
-                   '13-0', '17-3', '10-0', '18-4', '14-6', 'checkin_info',
-                   'type', 'business_id']
-csvify_to_file(checkin_headers,
-               "data/json_origin/yelp_academic_dataset_checkin.json",
-               "data/checkin.csv")
+# Convert the businesses file to csv
+business_file = "data/json_origin/yelp_academic_dataset_business.json"
+bus_headers = allkeys(business_file,
+                      noinc=["Ambience", "Good For", "hours", "Parking"])
+csvify_to_file(bus_headers, business_file, "data/business.csv")
 
-review_headers = ['funny', 'useful', 'cool', 'user_id',
-                  'review_id', 'text', 'business_id',
-                  'stars', 'date', 'type']
-csvify_to_file(review_headers,
-               "data/json_origin/yelp_academic_dataset_review.json",
-               "data/review.csv")
 
-tip_headers = ['user_id', 'text', 'business_id', 'likes', 'date', 'type']
-csvify_to_file(tip_headers,
-               "data/json_origin/yelp_academic_dataset_tip.json",
-               "data/tip.csv")
+# Convert the check-ins file to csv
+checkin_file = "data/json_origin/yelp_academic_dataset_checkin.json"
+checkin_headers = allkeys(checkin_file)
+csvify_to_file(checkin_headers, checkin_file, "data/checkin.csv")
 
-user_headers = ['yelping_since', 'votes', 'funny', 'useful', 'cool', 'votes',
-                'user_id', 'name', 'elite', 'type', 'compliments', 'profile',
-                'cute', 'funny', 'plain', 'writer', 'note', 'photos', 'hot',
-                'more', 'cool', 'compliments', 'fans', 'average_stars',
-                'review_count', 'friends']
-csvify_to_file(user_headers,
-               "data/json_origin/yelp_academic_dataset_user.json",
-               "data/user.csv")
+# Convert the reviews file to csv
+review_file = "data/json_origin/yelp_academic_dataset_review.json"
+review_headers = allkeys(review_file)
+csvify_to_file(review_headers, review_file, "data/review.csv")
+
+# Convert the tips file to csv
+tip_file = "data/json_origin/yelp_academic_dataset_tip.json"
+tip_headers = allkeys(tip_file)
+csvify_to_file(tip_headers, tip_file, "data/tip.csv")
+
+# Convert the users file to csv
+user_file = "data/json_origin/yelp_academic_dataset_user.json"
+user_headers = allkeys(user_file)
+csvify_to_file(user_headers, user_file, "data/user.csv")
